3bacdb6d32a840432db0c0843778d3529f173c11
DPS800 <deepzgrewalz@gmail.com>
2025-01-30 07:15:31 +0000

    Adding more text, style.css, and pie chart

--- script.js
+++ script.js
@@ -59,13 +59,13 @@ function start() {
                 '2023'
             ],
             datasets: [{
-                label: '% of Webpages Lost',
+                label: '% of Webpages Lost', fill: 'origin',
                 data: [
                     0, 3.8, 7.6, 11.4, 15.2, 19, 22.8, 26.6, 30.4, 34.2, 38
                 ],
                 borderWidth: 2,
-                backgroundColor: ['rgba(250, 172, 59, 0.5)'],
-                borderColor: ['rgba(250, 172, 59, 1)']
+                backgroundColor: ['rgba(255, 205, 39, 0.5)'],
+                borderColor: ['rgba(255, 205, 39, 1)']
             }]
         },
         options: {
@@ -80,6 +80,50 @@ function start() {
                     position: 'top',
                 },
             },
+        }
+    });
+
+    const pieCtx = document.getElementById('TLDPie');
+
+    new Chart(pieCtx, {
+        type: 'pie',
+        data: {
+            labels: [
+                '.com',
+                '.net',
+                '.ru',
+                '.org',
+                '.de',
+                '.jp',
+                '.co.uk',
+                '.ir',
+                'Other'
+            ],
+            datasets: [{
+                label: '% used',
+                data: [47.9, 5.22, 4.77, 4.61, 2.32, 1.49, 1.29, 1.24, 60.8],
+                borderWidth: 1,
+                backgroundColor: [
+                    'rgba(77, 101, 255, 1)',
+                    'rgba(170, 152, 202, 1)',
+                    'rgba(158, 206, 79, 1)',
+                    'rgba(0, 174, 242, 1)',
+                    'rgba(242, 100, 74, 1)',
+                    'rgba(250, 172, 59, 1)',
+                    'rgba(255, 205, 39, 1)',
+                    'rgba(252, 245, 230, 1)',
+                    'rgba(153, 153, 153, 1)'
+                ]
+            }]
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+
+            }
         }
     });
 }
